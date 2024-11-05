12c61e2569529db2e59c8576330f3a851d587664
se1987 <s.tsukuda1987@gmail.com>
2024-11-05 14:45:06 +0900

    CSSの適用

--- frontend/src/app/pages/posts/[id]/page.tsx
+++ frontend/src/app/pages/posts/[id]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Post as PostType } from "../../../models/types";
+import styles from "../../../styles/Post.module.css";
 
 type Props = {
   params: {
@@ -19,9 +20,10 @@ export default async function PostDetail({ params }: Props) {
   const post: PostType = await res.json();
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{post.title}</h1>
+      <div className={styles.date}>{post.created_at}</div>
+      <p className={styles.content}>{post.content}</p>
     </div>
   );
 }
