cde939f260471f77929ee846699b64b6600bec27
se1987 <s.tsukuda1987@gmail.com>
2024-11-11 15:12:00 +0900

    サーバーコンポーネントとクライアントコンポーネントの分離

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,72 +1,10 @@
-"use client";
-
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import styles from "./styles/Home.module.css";
-import { Post } from "./models/types";
-import axios from "axios";
+import HomeClient from "./pages/home/page";
 
 export default async function Home() {
   const res = await fetch("http://localhost:3001/api/v1/posts", {
     next: { revalidate: 60 * 60 * 24 }, // ISRの代替オプション
   });
   const posts = await res.json();
-  const router = useRouter();
 
-  // 更新ボタンの処理
-  const handleUpdate = async (post: Post) => {
-    router.push(`/pages/posts/edit-post/${post.id}`);
-  };
-
-  // 削除ボタンの処理
-  const handleDelete = async (id: any) => {
-    try {
-      const response = await axios.delete(
-        `http://localhost:3001/api/v1/posts/${id}`
-      );
-      router.reload();
-    } catch (err) {
-      console.error(err);
-      alert("削除に失敗しました");
-    }
-  };
-
-  return (
-    <div>
-      <main>
-        <div className={styles.homeContainer}>
-          <h1>Rails & Next.js Blog</h1>
-          <Link href="pages/posts/create-post" className={styles.createButton}>
-            Create new Post
-          </Link>
-
-          <div>
-            {posts.map((post: Post) => (
-              <div key={post.id} className={styles.postCard}>
-                <Link
-                  href={`pages/posts/detail/${post.id}`}
-                  className={styles.postCardBox}
-                >
-                  <h2>{post.title}</h2>
-                </Link>
-                <p>{post.content}</p>
-                <button
-                  className={styles.editButton}
-                  onClick={() => handleUpdate(post)}
-                >
-                  Edit
-                </button>
-                <button
-                  className={styles.deleteButton}
-                  onClick={() => handleDelete(post.id)}
-                >
-                  Delete
-                </button>
-              </div>
-            ))}
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+  return <HomeClient posts={posts} />;
 }
