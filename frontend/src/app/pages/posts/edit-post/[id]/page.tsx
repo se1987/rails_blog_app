0ead1f035573c12f03d065bc9f5bf72b22087907
se1987 <s.tsukuda1987@gmail.com>
2024-11-11 15:13:50 +0900

    更新ボタンの実装完了

--- frontend/src/app/pages/posts/edit-post/[id]/page.tsx
+++ frontend/src/app/pages/posts/edit-post/[id]/page.tsx
@@ -3,31 +3,46 @@
 import React, { useEffect } from "react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import axios from "axios";
-// import { Post as PostType } from "../../../models/types";
 import styles from "../../../../styles/Home.module.css";
+import { Post as PostType } from "../../../../models/types";
 import { useRouter } from "next/navigation";
 
-const EditPost = ({ params }: { params: { id: string } }) => {
+type EditPostProps = {
+  params: Promise<{ id: string }>; // URLパラメータから `id` を取得しPromiseとして定義
+};
+
+const EditPost = ({ params }: EditPostProps) => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [id, setId] = useState<string | null>(null); // 解決済みのIDを保存
   const router = useRouter();
 
   useEffect(() => {
+    const resolveParams = async () => {
+      const resolvedParams = await params; // params を解決
+      setId(resolvedParams.id);
+    };
+
+    resolveParams();
+  }, [params]);
+
+  useEffect(() => {
+    if (!id) return;
+
     const fetchPost = async () => {
-      const res = await fetch(
-        `http://localhost:3001/api/v1/posts/${params.id}`
-      );
-      const post = await res.json();
+      const res = await fetch(`http://localhost:3001/api/v1/posts/${id}`);
+      const post: PostType = await res.json();
       setTitle(post.title);
       setContent(post.content);
     };
     fetchPost();
-  }, [params.id]);
+  }, [id]);
 
+  // 更新ボタンの処理
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     try {
-      await axios.put(`http://localhost:3001/api/v1/posts`, {
+      await axios.put(`http://localhost:3001/api/v1/posts/${id}`, {
         title: title,
         content: content,
       });
@@ -46,6 +61,7 @@ const EditPost = ({ params }: { params: { id: string } }) => {
         <input
           type="text"
           id="title"
+          value={title}
           placeholder="タイトルを入力してください"
           className={styles.input}
           onChange={(e: ChangeEvent<HTMLInputElement>) =>
@@ -55,6 +71,7 @@ const EditPost = ({ params }: { params: { id: string } }) => {
         <label className={styles.label}>本文</label>
         <textarea
           id="content"
+          value={content}
           placeholder="本文を入力してください"
           className={styles.textarea}
           onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
