80f9a4e392a82fadbf879419c02a671a64c1b778
se1987 <s.tsukuda1987@gmail.com>
2024-11-13 11:52:06 +0900

    railsで画像URLを返すよう設定

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
     has_one_attached :image
-    validates :image, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+    # validates :image, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+
+    def as_json(options = {})
+        super(options).merge({ image_url: image.attached? ? Rails.application.routes.url_helpers.url_for(image) : nil })
+    end
 end
