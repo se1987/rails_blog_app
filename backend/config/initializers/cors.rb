7e40664807365ea9343fc977d88ab173157af813
se1987 <s.tsukuda1987@gmail.com>
2024-11-14 15:36:35 +0900

    CORS設定にActive Storageを許可

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,10 +7,15 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000"
+    origins "http://localhost:3000"
 
-    resource "*",
+    # Active Storage のみを対象とする
+    resource "/rails/active_storage/*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :options, :head]
+      
+    # resource "*",
+    #   headers: :any,
+    #   methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
