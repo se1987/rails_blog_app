ccb5354434d40c58e0cfd7c35727f71a0029d021
se1987 <s.tsukuda1987@gmail.com>
2024-11-14 14:35:40 +0900

    image_url の生成するメソッドas_json追加、モデル内でurl_forを使用するためRails.application.routes.url_helpers をインクルード、Active Storage を使ってモデルに1つの画像（image）を添付を可能に

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ApplicationRecord
-    has_one_attached :image
-    # validates :image, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+  has_one_attached :image
 
-    def as_json(options = {})
-        super(options).merge({ image_url: image.attached? ? Rails.application.routes.url_helpers.url_for(image) : nil })
-    end
+  include Rails.application.routes.url_helpers
+
+  def as_json(options = {})
+    super(options).merge(
+      image_url: image.attached? ? Rails.application.routes.url_helpers.url_for(image) : nil
+    )
+  end
 end
+
