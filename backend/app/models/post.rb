62435911744b62fa967f1839e86350e7dbd8c119
se1987 <s.tsukuda1987@gmail.com>
2024-11-18 14:30:30 +0900

    コメントの追記

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ApplicationRecord
+  # imageフィールドを追加して１件の投稿にひとつの画像ファイルを添付できるようマッピング
   has_one_attached :image
 
   include Rails.application.routes.url_helpers
-
+  # URLの生成を行い、そのURLをJSONレスポンスに含める
   def as_json(options = {})
     super(options).merge(
       image_url: image.attached? ? Rails.application.routes.url_helpers.url_for(image) : nil
