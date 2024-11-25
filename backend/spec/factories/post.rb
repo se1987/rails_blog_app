cfb1cf2087f81741405770dcc91a67b48d279914
se1987 <s.tsukuda1987@gmail.com>
2024-11-25 16:30:05 +0900

    Model Specのテストコード作成

--- backend/spec/factories/post.rb
+++ backend/spec/factories/post.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
     factory :post do
         # 英数字のランダムな文字列を生成する（100文字)
-        content { Faker::Lorem.characters(number:100)}
+        title { "有効なタイトル" }
+        content { "Rspecテストの投稿です。有効なコンテンツ。"}
     end
 end
--- backend/spec/models/post_spec.rb
+++ backend/spec/models/post_spec.rb
@@ -1,16 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Post do
-    let(:post) { build(:post) } expect(post).to be_valid
-    it "is valid with a valid content" do
-        expect(tweet).to be_valid
+RSpec.describe Post, type: :model do
+  let(:post) { build(:post) } # FactoryBot を使用してインスタンスを生成
+
+  describe 'バリデーションのテスト' do
+    it "有効なコンテンツを持つ場合に有効である" do
+      post.content = '有効なコンテンツ'
+      expect(post).to be_valid
     end
-    it "is not valid without a content" do
-        tweet.content = ''
-        expect(tweet).to_not be_valid
+
+    it "コンテンツが空の場合に無効である" do
+      post.content = ''
+      expect(post).to_not be_valid
     end
-    it "is not valid with a content longer than 140" do
-        tweet.content = Faker::Lorem.characters(number: 9)
-        expect(tweet).to_not be_valid
+
+    it "コンテンツが10文字未満の場合に無効である" do
+      post.content = 'a' * 9
+      expect(post).to_not be_valid
     end
+  end
 end
