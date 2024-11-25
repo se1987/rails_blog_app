b15d28789c64163dd30d3775a7047f0ec556a57e
se1987 <s.tsukuda1987@gmail.com>
2024-11-25 13:54:13 +0900

    postモデルのバリデーションチェック追記

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   # imageフィールドを追加して１件の投稿にひとつの画像ファイルを添付できるようマッピング
   has_one_attached :image
+  # バリデーション
+  validates :title, presence: true
+  validates :content, length: { minimum: 10 }
 end
-
