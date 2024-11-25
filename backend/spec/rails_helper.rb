ac04ea6ca0e64e7a20b55f5f74bcbe0037f86985
se1987 <s.tsukuda1987@gmail.com>
2024-11-25 11:03:23 +0900

    テスト用オブジェクトを生成するときにcreate()やbuild()を使えるようになるシンタックスシュガーの設定

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -61,4 +61,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # テスト用オブジェクトを生成するときにcreate()やbuild()を使えるようになるシンタックスシュガーの設定
+  config.include FactoryBot::Syntax::Methods
 end
