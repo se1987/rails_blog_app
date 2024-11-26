fb60c1c2f9f200e09c91bd9882dcd28b1a46460a
se1987 <s.tsukuda1987@gmail.com>
2024-11-26 15:01:54 +0900

    テストコードの修正、spec/rails_helper.rbの設定を変更

--- backend/spec/models/post_spec.rb
+++ backend/spec/models/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:post) { build(:post) } # FactoryBot を使用してインスタンスを生成
+  let(:post) { FactoryBot.build(:post) } # FactoryBot を使用してインスタンスを生成
 
   describe 'バリデーションのテスト' do
     it "有効なコンテンツを持つ場合に有効である" do
